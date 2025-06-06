382e730d2695efb02eb988c54f165f56cca58c6f
lavecat <lirus12345.lave@gmail.com>
2025-06-06 22:55:58 +0200

    Rest API finish

--- kxspy/rest.py
+++ kxspy/rest.py
@@ -17,7 +17,7 @@ class RestApi:
     def __init__(self, kxs_network_rest__url: str = "https://network.kxs.rip") -> None:
         self.rest_uri = kxs_network_rest__url
 
-    async def request(self, method: str, rout: str, data: dict = {}) -> dict or None:
+    async def request(self, method: str, rout: str, data: dict = {}) -> dict or str:
         """
         This function makes a request to the kxs network REST API.
 
@@ -39,6 +39,8 @@ class RestApi:
         async with aiohttp.ClientSession() as session:
             async with session.request(method, self.rest_uri + rout,json=data) as _response:
                 _LOG.debug(f"{method} {self.rest_uri + rout}")
+                if _response.content_type == "text/plain":
+                    return await _response.text()
 
                 response = await _response.json()
 
@@ -58,6 +60,34 @@ class RestApi:
         :class:`dict`
             The response from the request.
         """
-
         res = await self.request("GET", "/online-count")
+        return res
+
+    async def ig_count(self, gameId: str) -> dict:
+        """
+        This function makes a request to the kxs network REST API.
+
+        Parameters
+        ---------
+        gameId: :class:`str`
+            The game id.
+
+        Returns
+        -------
+        :class:`dict`
+            The response from the request.
+        """
+        res = await self.request("GET", f"/ig-count/{gameId}")
+        return res
+
+    async def getLatestVersion(self) -> str:
+        """
+        This function makes a request to the kxs network REST API.
+
+        Returns
+        -------
+        :class:`str`
+            The response from the request.
+        """
+        res = await self.request("GET", "/getLatestVersion")
         return res
