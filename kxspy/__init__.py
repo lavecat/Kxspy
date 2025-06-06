fadb0c4de35703fff68abb82e8408c231bde2c59
lavecat <lirus12345.lave@gmail.com>
2025-06-06 21:58:48 +0200

    Update __init__.py

---
+++ kxspy/__init__.py
@@ -0,0 +1,13 @@
+"""
+kxspy
+---------
+:copyright: (c) 2025 lavecat
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'kxspy'
+__author__ = 'lavecat'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2025-present lavecat'
+__version__ = '1.0.0'
+
