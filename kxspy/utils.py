e5429b16d03a6d595b7e726d3c8664b1c04e30c8
lavecat <lirus12345.lave@gmail.com>
2025-06-14 13:01:02 +0200

    Huh?

--- kxspy/utils.py
+++ kxspy/utils.py
@@ -4,4 +4,4 @@ import logging
 _LOG = logging.getLogger("kxspy.utils")
 
 def get_random_username():
-    return "kxspy_" + ''.join(random.choice("0123456789abcdef") for _ in range(7))
+    return "kxspy_" + ''.join(random.choice("0123456789abcdef") for _ in range(10))
